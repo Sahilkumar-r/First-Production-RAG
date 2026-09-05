a48f8fa5d22a2503faa27fcbbca5321581644d05
Sahilkumar-r <sahilrathi.k@gmail.com>
2026-09-05 16:37:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import logging
 import os
 import uuid
 import datetime
-from fastapi import FastAPI, Body
 import inngest
 import inngest.fast_api
 from dotenv import load_dotenv
 from groq import Groq 
-
+from fastapi import FastAPI, Body, UploadFile, File
 from data_loader import load_and_chunk_pdf, embed_texts
 from vector_db import QdrantStorage
 from custom_types import RAQQueryResult, RAGSearchResult, RAGUpsertResult, RAGChunkAndSrc
